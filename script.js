12472932f8d09d994ac5c03301e64cb2e54c1121
ajuteixeira <teixeirajuuh@gmail.com>
2025-09-12 20:58:19 -0300

    ajustando links, adicionando botão de voltar ao topo e rolagem suave

--- script.js
+++ script.js
@@ -28,3 +28,20 @@ next.addEventListener('click', () => {
 prev.addEventListener('click', () => {
   carousel.scrollBy({ left: -220, behavior: 'smooth' });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const backToTop = document.getElementById('back-to-top');
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 300) {
+      backToTop.style.display = 'block';
+    } else {
+      backToTop.style.display = 'none';
+    }
+  });
+  backToTop.addEventListener('click', function () {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth', // efeito suave
+    });
+  });
+});
